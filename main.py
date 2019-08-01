d469d99d84acc18922bcee6e022dfe7b9a41d2ba
Ryan Haas <49294304+haasr@users.noreply.github.com>
2019-07-31 21:09:09 -0400

    Update main.py
    * Added exception handling for requests exceptions.
    * Added recognition to phrases asking for weather or forecast to use the new weather_util module to get weather/forecast data for a location.
    * Updated send_snap_reply function so that it downloads the image from the URL originally passed into the POST request and saves the image as a random name to the index directory of my Apache server. This is a workaround for an issue I was having getting new images to post from the same URL wherein the first time the URL was used the correct image was posted; however, every other time the URL was used to post an image, even when the image was completely different, as long as the same URL was used the same image would post.

--- main.py
+++ main.py
@@ -1,96 +1,154 @@
 # main.py
 
-# Rudimentary program which interfaces with the GroupMe API to scan through given 
-# GroupMe group and look for key words which indicate queries to the GroupMe bot. 
-# The program will then use post requests to post a response. Provides some useful
-# data from MotionEye cam on LAN and makes basic conversation using the pops_calls
-# module. 
-
-# My code resembles the code from http://sweb.uky.edu/~jtba252/index.php/2017/09/13/how-to-write-a-groupme-bot-using-python/
-# which I read and followed to learn how to interface with the GroupMe API in Python. Thanks for a great tutorial.
-
-
+import os
+import random
 import re
 import requests
 import time
 import pops_calls
+from pops_utils import weather_util
 
 BOT_ID = 'INSERT BOT ID HERE'
 GROUP_ID = 'INSERT GROUP ID HERE'
 ACCESS_TOKEN = 'INSERT ACCESS TOKEN HERE'
 POST_URL = 'https://api.groupme.com/v3/bots/post'
 UPLOADS_URL = 'INSERT DROPBOX OR DRIVE URL TO UPLOADS HERE'
-MOTIONEYE_IP = '192.168.1.104'
+MOTIONEYE_IP = 'INSERT IP ADDRESS OF MOTIONEYE CAM HERE'
 
 request_params = { 'token': ACCESS_TOKEN }
 
 def send_reply(reply, message):
-    post_params = { 'bot_id': BOT_ID, 'text': reply } 
+    post_params = { 'bot_id': BOT_ID, 'text': reply }
     req = requests.post(POST_URL, params = post_params)
-    
+
     request_params['since_id'] = message['id']
     print('Sent reply: ' + reply)
     print(req) # Print http response.
 
 
 def send_snap_reply(message):
-    # I couldn't figure out how to send an image following Groupme's API documentation.
-    # Kept getting http response 400. The solution was not to use 'attachments' in the
-    # post parameters but 'picture_url' as shown here:
-    # https://stackoverflow.com/questions/53807009/groupme-bot-is-not-sending-images-along-with-its-text
-     
-    post_params = { 'bot_id': BOT_ID, 'text': '' } 
-    # Pass URL to current snapshot from MotionEye camera to the data field.
+    # GroupMe's API seemed to cache the image data from the URL i gave it 
+    # (for example, http://192.168.1.104/picture/1/current/') which would 
+    # change the image data it contained to be a current snapshot from the
+    # MotionEye cam but as long as the same URL was used, it posted the
+    # same image. Solution was set up an apache server on my RPi (hosting
+    # the bot) to download the image from the link above to a random name 
+    # in the html root dir (to make URL unique) and pass that to GroupMe
+    # in POST request.
+    
+    # Location of apache server's index page and dir where images are stored
+    # so they can be accessed by a local url.
+    image_dir = '/var/www/html'
+    test = os.listdir(image_dir)
+
+    for item in test:
+        # Remove all old jpegs from previous downloads.
+        if (item.endswith('.jpeg')): 
+            os.remove(os.path.join(image_dir, item))
+
+    # Goal here is basically to create an image name that will never be used again.
+    # Workaround for groupme api caching first image from url and never sending
+    # new image even after conent in 'current.jpeg', for example, had changed.
+    filename = str(random.randint(0, 90000000)) + str(random.randint(0, 90000000)) + '.jpeg'
+    f = open('/var/www/html/' + filename, 'wb')
+    f.write(requests.get('http://192.168.1.104/picture/1/current/').content)
+    f.close()
+
+    post_params = { 'bot_id': BOT_ID, 'text': '' }
     post_data = {'text': 'current.jpeg', 'picture_url': 'http://' + MOTIONEYE_IP + '/picture/1/current/'}
     req = requests.post(POST_URL, params = post_params, data = post_data)
-    
+
     request_params['since_id'] = message['id']
     print('Sent most recent image snap')
     print(req) # Print http response.
 
 
 def main():
+    # Parse in all the location data from file.
+    # "worldcities.csv" https://simplemaps.com/data/world-cities by Pareto Software, LLC is licensed under CC BY 4.0
+    # File is in "./pops_utils/weather_data".
+    weather_util.parse_location_data()
 
     while True:
+        try:
+            response = requests.get('https://api.groupme.com/v3/groups/' + GROUP_ID 
+                    + '/messages', params = request_params)
 
-        print('\n####################\n# Bot: Popsicle \n# Group: Dev Testing'
-                + '\n# Status: Listening\n ###################')
-    
-        response = requests.get('https://api.groupme.com/v3/groups/' + GROUP_ID 
-                + '/messages', params = request_params)
+            if (response.status_code == 200):
+                response_messages = response.json()['response']['messages']
 
-        if (response.status_code == 200):
-            response_messages = response.json()['response']['messages']
+                print('\n####################\n# Bot: Pops \n# Group: Pops Alerts'
+                    + '\n# Status: Listening\n ###################')
 
-            # Iterate through each message, checking its text
-            for message in response_messages:
-                if not (message['text'] == None):
+                # Iterate through each message, checking its text
+                for message in response_messages:
+                    if not (message['text'] == None):
+                        # Test for Weather query first
+                        # Forecast searches before weather because ppl may say "Whats the weather forecast"
+                        str_search = re.search('what.*s.*forecast', message['text'].lower())
+                        if (str_search):
+                            reply = weather_util.get_forecast(message['text'])
+                            send_reply(reply, message)
+                            break
 
-                    # If someone tags Pops, pops_calls handles the reply.
-                    if ('@pops' in message['text'].lower()):
-                        reply = pops_calls.get_reply(message)
-                        send_reply(reply, message)
-                        break
+                        str_search = re.search('tell.*forecast', message['text'].lower())
+                        if (str_search):
+                            reply = weather_util.get_forecast(message['text'])
+                            send_reply(reply, message)
+                            break
 
-                    # Key words to get info from MotionEye camera; popcam_calls handles these. 
-                    elif ('ALERT!!' in message['text']):
-                        send_snap_reply(message)
-                        break
-                        
-                    elif (message['text'] == 'SNAP'):
-                        send_snap_reply(message)
-                        break
+                        str_search = re.search('what.*s.*weather', message['text'].lower())
+                        if (str_search):
+                            reply = weather_util.get_weather(message['text'])
+                            send_reply(reply, message)
+                            break
 
-                    elif (message['text'] == 'STREAM'):
-                        send_reply(reply, MOTIONEYE_IP)
-                        break
+                        str_search = re.search('tell.*weather', message['text'].lower())
+                        if (str_search):
+                            reply = weather_util.get_weather(message['text'])
+                            send_reply(reply, message)
+                            break
 
-                    elif (message['text'] == 'UPLOADS'):
-                        reply = UPLOADS_URL
-                        send_reply(reply, message)
-                        break
-                    else:
-                        pass   
-                
+                        # If someone tags Pops (and not to ask weather), pops_calls handles the reply.
+                        if ('@pops' in message['text'].lower()):
+                            reply = pops_calls.get_reply(message)
+                            send_reply(reply, message)
+                            break
+
+                        # Key words to get info from MotionEye camera; popcam_calls handles these. 
+
+                        # I have an IFTTT webhook which when called by MotionEye, posts 'ALERT!!...'
+                        # when motion is detected. Then my bot knows to post a current snapshot.
+                        elif ('ALERT!!' in message['text']):
+                            send_snap_reply(message)
+                            break
+
+                        elif (message['text'] == 'SNAP'):
+                            send_snap_reply(message)
+                            break
+
+                        elif (message['text'] == 'STREAM'):
+                            send_reply(MOTIONEYE_IP, message)
+                            break
+
+                        elif (message['text'] == 'UPLOADS'):
+                            send_reply(UPLOADS_URL, message)
+                            break
+
+        except requests.exceptions.ConnectionError as e:
+            print('No connection')
+            print(e)
+
+        except requests.exceptions.Timeout as e:
+            print('Connection timed out')
+            print(e)
+
+        except requests.exceptions.RequestException as e:
+            print('A request error ocurred')
+            print(e)
+
+        except:
+            print('An error occurred')
+
         time.sleep(5)
 main()
