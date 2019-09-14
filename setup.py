d5dbf1d069510a444f3d3399e564e596bb5d7aef
Ryan Haas <haasr@protonmail.com>
2019-09-14 11:53:13 -0400

    New package version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst", "r") as readme:
 
 setup(
     name='motioneyebot',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     author='Ryan Haas',
     author_email='haasrr@etsu.edu',
